835f86b798b058a1b99d54c3027844023a935e28
s-ohtsuki1 <secom.24h@gmail.com>
2021-04-27 01:13:38 +0900

    記事データをDBから取得

---
+++ src/app/Models/Article.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    /*
+     * Get the user that owns the Articles
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo('App\User');
+    }
+}
