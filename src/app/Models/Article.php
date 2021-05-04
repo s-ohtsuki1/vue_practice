b459701cee87b4ae2312a569b2e30a2439604d50
s-ohtsuki1 <secom.24h@gmail.com>
2021-05-04 15:51:15 +0900

    いいね機能追加

--- src/app/Models/Article.php
+++ src/app/Models/Article.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -20,5 +22,22 @@ class Article extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
+    }
+
+    public function getCountLikesAttribute(): int
+    {
+        return $this->likes->count();
     }
 }
